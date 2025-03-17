5875d27d38826f49c9fa4dd66ee81c48f00ce659
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-17 09:46:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const redButton = document.getElementById('redButton');
 // Add event listener to the red button
 redButton.addEventListener('click', function() {
     sessionStorage.setItem('redButtonClickedTime', Date.now());
-    window.location.href = 'https://chilsihooveek.net/4/8442621';
+    window.location.href = 'https://www.effectiveratecpm.com/xxigxy68w?key=5269a7d92a85ade15a1f71703c0b0739';
 });
