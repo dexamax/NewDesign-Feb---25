c4bbd4cd9a00dbc43a21e8c31fdce40f165953c6
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-17 10:16:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const redButton = document.getElementById('redButton');
 // Add event listener to the red button
 redButton.addEventListener('click', function() {
     sessionStorage.setItem('redButtonClickedTime', Date.now());
-    window.location.href = 'https://www.effectiveratecpm.com/xxigxy68w?key=5269a7d92a85ade15a1f71703c0b0739';
+    window.location.href = 'https://aggravatingoil.com/bb3.Vq0NP/3hpiv/bqmBVTJnZFDs0B1/NNTEkR2gNLDHQxwVLFTKUu1bO/TiYW0WN_DiEc';
 });
