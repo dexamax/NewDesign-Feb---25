74d5941f6f0c64c370ed09fcfa044354499c638b
dexamax <166790203+dexamax@users.noreply.github.com>
2025-03-20 07:55:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const redButton = document.getElementById('redButton');
 // Add event listener to the red button
 redButton.addEventListener('click', function() {
     sessionStorage.setItem('redButtonClickedTime', Date.now());
-    window.location.href = 'https://ptukseew.top/4/8442621';
+    window.location.href = 'https://aggravatingoil.com/bq3FVR0wP.3Np-v/b_m/VEJWZbDP0G1GNJTBk/2/NODbQ/wbL/TxUP1AOMTfYQ0MNlD/EV';
 });
